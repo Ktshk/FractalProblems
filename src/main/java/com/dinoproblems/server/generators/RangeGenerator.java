2a04434058ac21a44d2a49425f1b52d764db33b9
Egor Ovsyannikov <e.a.ovsya@gmail.com>
2019-04-09 16:54:33 +0300

    RangeGenerator tts implementation Minor changes in OrdinalNumber and Adjective

--- src/main/java/com/dinoproblems/server/generators/RangeGenerator.java
+++ src/main/java/com/dinoproblems/server/generators/RangeGenerator.java
@@ -4,6 +4,9 @@ import com.dinoproblems.server.Problem;
 import com.dinoproblems.server.ProblemGenerator;
 import com.dinoproblems.server.ProblemWithPossibleTextAnswers;
 import com.google.common.collect.Sets;
+import com.dinoproblems.server.utils.NumberWord;
+import com.dinoproblems.server.utils.ProblemTextBuilder;
+import com.dinoproblems.server.utils.OrdinalNumber;
 
 
 import java.util.Set;
@@ -14,6 +17,7 @@ import static com.dinoproblems.server.ProblemCollection.RANGE;
 import static com.dinoproblems.server.utils.Dictionary.PAGE;
 import static com.dinoproblems.server.utils.GeneratorUtils.Gender.MASCULINE;
 import static com.dinoproblems.server.utils.GeneratorUtils.Gender.NEUTER;
+import static com.dinoproblems.server.utils.GeneratorUtils.Gender.FEMININE;
 import static com.dinoproblems.server.utils.GeneratorUtils.getNumWithString;
 import static com.dinoproblems.server.utils.GeneratorUtils.randomInt;
 import static com.dinoproblems.server.Problem.Difficulty.EASY;
@@ -30,7 +34,7 @@ public class RangeGenerator implements ProblemGenerator {
 
         final int answer;
         final String hero;
-        String text;
+        ProblemTextBuilder text = new ProblemTextBuilder();
         String hint;
         Set<String> possibleTextAnswers;
 
@@ -44,7 +48,10 @@ public class RangeGenerator implements ProblemGenerator {
             answer = second - first + 1;
             final String numsWithText = getNumWithString(answer, "число", "числа", "чисел", NEUTER);
             possibleTextAnswers = Sets.newHashSet(numsWithText);
-            text = "Сколько чисел от " + first + " до " + second + "?";
+            text.append("Сколько чисел от ");
+            text.append(String.valueOf(first),NumberWord.getStringForNumber(first, NEUTER, GeneratorUtils.Case.GENITIVE));
+            text.append(" до ");
+            text.append(String.valueOf(second),NumberWord.getStringForNumber(second, NEUTER, GeneratorUtils.Case.GENITIVE)).append("?");
             hint = "Попробуйте отнять от второго числа первое, но не забывайте, что первое число тоже входит в диапазон чисел.";
         } else if (scenario == 1) {
 
@@ -54,22 +61,29 @@ public class RangeGenerator implements ProblemGenerator {
             answer = second + first - 1;
             final String pagesWithText = getNumWithString(second, PAGE, GeneratorUtils.Case.ACCUSATIVE);
             possibleTextAnswers = Sets.newHashSet(answer + " странице");
-            text = hero + " начала читать книгу с " + first + " страницы и прочитала ровно " + pagesWithText +
-                    ". На какой странице " + hero + " закончила?";
+            text.append(hero + " начала читать книгу с ").append(String.valueOf(first), OrdinalNumber.getOrdinalTwoDigitNum(first).getGenitiveForm(FEMININE));
+            text.append(" страницы и прочитала ровно " + pagesWithText +
+                    ". На какой странице " + hero + " закончила?");
             hint = "Попробуйте сложить страницы, но не забывайте, что страница, с которой " + hero +
                     " начала читать тоже входит в количество прочитанных страниц.";
         } else if (scenario == 2) {
             String[] booksource = new String[]{" в библиотеке ", " у подруги ", " у друга ", " у учителя "};
             String book = booksource[randomInt(0, booksource.length)];
             hero = heroes[randomInt(0, heroes.length)];
-            first = difficulty == EASY ? randomInt(1, 7) : randomInt(1, 11);
+            first = difficulty == EASY ? randomInt(1, 7) : randomInt(1, 12);
+            if(first % 2 != 0) {
+                first++;
+            }
             second = difficulty == EASY ? randomInt(11, 20) : randomInt(28, 41);
+            if(second % 2 == 0) {
+                second++;
+            }
             answer = second - first - 1;
             final String pagesWithText = getNumWithString(answer, PAGE);
             possibleTextAnswers = Sets.newHashSet(pagesWithText);
-            text = hero + " взяла" + book + "книгу. В середине книги она обнаружила, что после страницы " + first +
-                    " сразу идет страница " + second + ". Сколько страниц было вырвано из книги?";
-            hint = "Попробуйте вычесть страницы, но не забывайте, что " + first + " " + second +
+            text.append(hero + " взяла" + book + "книгу. В середине книги она обнаружила, что после страницы " + first +
+                    " сразу идет страница " + second + ". Сколько страниц было вырвано из книги?");
+            hint = "Попробуйте вычесть страницы, но не забывайте, что страницы " + first + " и " + second +
                     " не входят в количество вырванных страниц.";
         } else {
             String[] months = new String[]{" января ", " марта ", " мая ", " июля ", " августа ", " октября "};
@@ -84,12 +98,14 @@ public class RangeGenerator implements ProblemGenerator {
             answer = 31 - first + second + 1;
             final String daysWithText = getNumWithString(answer, "день", "дня", "дней", MASCULINE);
             possibleTextAnswers = Sets.newHashSet(daysWithText);
-            text = "На дверях библиотеки " + hero + " прочла: «С " + first + beginMonth + "по " + second + endMonth +
-                    "библиотека не работает». Сколько дней отпуск у библиотекаря? (В" + monthsInstr[monthIndex] + "31 день)";
+            text.append("На дверях библиотеки " + hero + " прочла: «С ");
+            text.append(String.valueOf(first),OrdinalNumber.getOrdinalTwoDigitNum(first).getGenitiveForm(MASCULINE));
+            text.append(beginMonth + "по ").append(String.valueOf(second),  OrdinalNumber.getOrdinalTwoDigitNum(second).getNominative(NEUTER));
+            text.append(endMonth + "библиотека не работает». Сколько дней отпуск у библиотекаря? (В" + monthsInstr[monthIndex] + "31 день)");
             hint = "Попробуйте из количества дней в" + monthsInstr[monthIndex] + "вычесть дату начала отпуска. " +
                     "Не забывайте, что день начала и день конца отпуска входят в количество дней в отпуске.";
         }
-        return new ProblemWithPossibleTextAnswers(text, answer, RANGE, possibleTextAnswers, hint);
+        return new ProblemWithPossibleTextAnswers(text.getText(), text.getTTS(), answer, RANGE, possibleTextAnswers, hint);
     }
 
     @Override
--- src/main/java/com/dinoproblems/server/utils/OrdinalNumber.java
+++ src/main/java/com/dinoproblems/server/utils/OrdinalNumber.java
@@ -3,6 +3,9 @@ package com.dinoproblems.server.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.dinoproblems.server.utils.GeneratorUtils.Gender.FEMININE;
+
+
 /**
  * Created by Katushka on 04.03.2019.
  */
@@ -29,11 +32,61 @@ public class OrdinalNumber extends Adjective {
             "9", "девятого", "девятой", "9");
     public static final OrdinalNumber TENTH = new OrdinalNumber(10, "десятый", "десятая", "десятое",
             "10", "десятого", "десятой", "10");
+    public static final OrdinalNumber ELEVENTH = new OrdinalNumber(11, "одиннадцатый", "одиннадцатая", "одиннадцатое",
+            "11", "одиннадцатого", "одиннадцатой", "11");
+    public static final OrdinalNumber TWELFTH = new OrdinalNumber(12, "двенадцатый", "двенадцатая", "двенадцатое",
+            "12", "двенадцатого", "двенадцатой", "12");
+    public static final OrdinalNumber THIRTEENTH = new OrdinalNumber(13, "тринадцатый", "тринадцатая", "тринадцатое",
+            "13", "тринадцатого", "тринадцатой", "13");
+    public static final OrdinalNumber FOURTEENTH = new OrdinalNumber(14, "четырнадцатый", "четырнадцатая", "четырнадцатое",
+            "14", "четырнадцатого", "четырнадцатой", "14");
+    public static final OrdinalNumber FIFTEENTH = new OrdinalNumber(15, "пятнадцатый", "пятнадцатая", "пятнадцатое",
+            "15", "пятнадцатого", "пятнадцатой", "15");
+    public static final OrdinalNumber SIXTEENTH = new OrdinalNumber(16, "шестнцадцатый", "шестнадцатая", "шестнадцатое",
+            "16", "шестнадцатого", "шестнадцатой", "16");
+    public static final OrdinalNumber SEVENTEENTH = new OrdinalNumber(17, "семнадцатый", "семнадцатая", "семнадцатое",
+            "17", "семнадцатого", "семнадцатой", "17");
+    public static final OrdinalNumber EIGHTEENTH = new OrdinalNumber(18, "восемнадцатый", "восемнадцатая", "восемнадцатое",
+            "18", "восемнадцатого", "восемнадцатой", "18");
+    public static final OrdinalNumber NINETEENTH = new OrdinalNumber(19, "девятнадцатый", "девятнадцатая", "девятнадцатое",
+            "19", "девятнадцатого", "девятнадцатой", "19");
+    public static final OrdinalNumber TWENTIETH = new OrdinalNumber(20, "двадцатый", "двадцатая", "двадцатое",
+            "20", "двадцатого", "двадцатой", "20");
+    public static final OrdinalNumber THIRTIETH = new OrdinalNumber(30, "тридцатый", "тридцатая", "тридцатое",
+            "30", "тридцатого", "тридцатой", "20");
+    public static final OrdinalNumber FORTIETH = new OrdinalNumber(40, "сороковой", "сороковая", "сороковое",
+            "40", "сорокового", "сороковой", "40");
+
+    private static final String[] TENS = {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят",
+            "восемьдесят", "девяносто"};
 
     private OrdinalNumber(int num, String nomMasculine, String nomFeminine, String nomNeuter, String countingForm, String genMasculine, String genFeminine, String plural) {
         super(nomMasculine, nomFeminine, nomNeuter, countingForm, genMasculine, genFeminine, plural);
 
         numbers.put(num, this);
+    }
+
+    public static OrdinalNumber getOrdinalTwoDigitNum(int num)
+    {
+        if(num < 21 || num % 10 == 0) {
+            return numbers.get(num);
+        }
+        int firstDigit = num / 10;
+        int secondDigit = num % 10;
+        String digit = TENS[firstDigit];
+        digit += " ";
+        OrdinalNumber secondOrdinalDigit = numbers.get(secondDigit);
+        OrdinalNumber result = new OrdinalNumber
+                (
+                        num,
+                        digit + secondOrdinalDigit.getNominativeMasculine(),
+                        digit + secondOrdinalDigit.getNominativeFeminine(),
+                        digit + secondOrdinalDigit.getNominativeNeuter(),
+                        digit + secondOrdinalDigit.getCountingForm(),
+                        digit + secondOrdinalDigit.getGenitiveMasculine(),
+                        digit + secondOrdinalDigit.getGenitiveFeminine(),
+                        Integer.toString(num));
+        return  result;
     }
 
     public static OrdinalNumber number(int num) {
